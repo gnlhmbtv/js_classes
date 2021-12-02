72d19bff010b1c00c3f703ba85961e3f56d8f153
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-12-02 09:10:13 +0400

    refactor:assign

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 2. Bir class yaradirsiz ve o classin icerisinde plus, minus, multiply, divide functionlari olur.         
 // var result = new CustomMatch(100).plus(6).minus(30).multiply(3).divide(2)
 //  resultin neticesi bu yazilisa gore 50+6-30*3/2 mentiqi ile (riyazi prinsibi unudun,vurma
-//   bolme onceliyine ehtiyac yoxdu) 114 olmalidi;
+//   bolme onceliyine ehtiyac yoxdu) 114 olmalidi ;
 
 class CustomMatch{
     constructor(num){
